dae0671deac5623e8d56dc72544f6446c2d3a93b
Marcin Ostrowski <marcin@sology.eu>
2014-05-13 22:42:48 +0200

    Spaces fix

--- chromosome.rb
+++ chromosome.rb
@@ -4,38 +4,38 @@ MUTATION_RATE = 0.001
 
 class Chromosome
 
-	attr_accessor :genes
+  attr_accessor :genes
 
-	def initialize genes = nil
-		genes ? self.genes = genes : self.genes = (1..GENE_SIZE).map{ rand 3 }.join
-	end
+  def initialize genes = nil
+    genes ? self.genes = genes : self.genes = (1..GENE_SIZE).map{ rand 3 }.join
+  end
 
-	def fitness
-		genes.count('2') + genes.count('1')/2
-	end
+  def fitness
+    genes.count('2') + genes.count('1')/2
+  end
 
-	def mutate!
-		mutated = []
-		genes.split('').each do |gene|
-			if rand < MUTATION_RATE
-				gene += 1
-				gene %= 3
-			end
-		end
-		mutated << gene
-		self.genes = mutated.join
-	end
-			
-	def & other
-		split = rand(genes.length + 1)
-		child1 = genes[0, split] + other.genes[split, other.genes.length]
-		child2 = other.genes[0, split] + genes[split, genes.length]
-		[Chromosome.new(child1), Chromosome.new(child2)]
-	end
+  def mutate!
+    mutated = []
+    genes.split('').each do |gene|
+      if rand < MUTATION_RATE
+        gene += 1
+        gene %= 3
+      end
+    end
+    mutated << gene
+    self.genes = mutated.join
+  end
+      
+  def & other
+    split = rand(genes.length + 1)
+    child1 = genes[0, split] + other.genes[split, other.genes.length]
+    child2 = other.genes[0, split] + genes[split, genes.length]
+    [Chromosome.new(child1), Chromosome.new(child2)]
+  end
 
-	def to_s
-		genes
-	end
+  def to_s
+    genes
+  end
 end
 c1 = Chromosome.new
 c2 = Chromosome.new
